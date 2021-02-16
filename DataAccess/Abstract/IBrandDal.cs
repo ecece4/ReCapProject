886b70189f3e53b66fe96ae89e71ccebb1efbf10
ecece4 <irtel_ecenaz@hotmail.com>
2021-02-17 00:11:46 +0300

    entity framework entegre edildi

--- DataAccess/Abstract/IBrandDal.cs
+++ DataAccess/Abstract/IBrandDal.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace DataAccess.Abstract
 {
-  public  interface IBrandDal:ICarDal<Brand>
+  public  interface IBrandDal : IEntityRepository<Brand>
     {
       
     }
--- DataAccess/Abstract/IEntityRepository.cs
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -6,11 +6,11 @@ using System.Text;
 
 namespace DataAccess.Abstract
 {
-    public interface ICarDal<T> where T: class, IEntity
+    public interface IEntityRepository<T> where T: class, IEntity
     {
        
         List<T> GetAll(Expression<Func<T,bool>> filter=null);
-       T Get(Expression<Func<T, bool>> filter);
+        T Get(Expression<Func<T, bool>> filter);
         void Add(T entity);
         void Delete(T entity);
         void Update(T entity);
